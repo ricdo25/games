59683d1125f5227fddefe4b09c614e3041cde81e
quality-sft <rrjuanchis@gmail.com>
2026-09-01 21:42:54 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANTE: reemplazá esto por el nombre EXACTO de tu repositorio en GitHub.
   // Ej: si tu repo se llama "mi-galeria-juegos", queda base: '/mi-galeria-juegos/'
-  base: '/NOMBRE-DE-TU-REPO/',
+  base: '/games/',
 })
